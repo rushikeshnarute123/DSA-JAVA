c3962f89c6e72361fcbec407b842c5503a422491
rushikeshnarute123 <rushikeshnarute3135@gmail.com>
2025-12-04 20:23:29 +0530

    aa
    aa

---
+++ 1 print z/z.java
@@ -0,0 +1,22 @@
+class z{
+    public static void main(String[] args) {
+        int n=5;
+        for (int i = 1; i <= n; i++) {
+            System.out.print("*");
+        }
+        System.err.println();
+
+
+        for(int i=n-1; i>0; i--){
+            for(int j=1; j<i; j++){
+                System.out.print(" ");
+            }
+            System.out.println("*");
+        }
+
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print("*");
+        }
+    }
+}
